18ce52fd985e8549749b6065c5b6c10e5e34e4e0
Code Sandbox <hello@codesandbox.io>
2022-11-20 14:35:31 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dd5de32782f6971f88d835229b60e79e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0787cf114af7d56ccce",
+    "url": "https://.github.io/csb-6lk4w0/static/js/main.66254e47.chunk.js"
+  },
+  {
+    "revision": "be070e6ac22791d54a28",
+    "url": "https://.github.io/csb-6lk4w0/static/js/runtime~main.7f58c34a.js"
+  },
+  {
+    "revision": "f8d85d82e4cc56b44fbd",
+    "url": "https://.github.io/csb-6lk4w0/static/js/2.3e5232d3.chunk.js"
+  },
+  {
+    "revision": "965ed122dfa2339b2aa59688aa75d39a",
+    "url": "https://.github.io/csb-6lk4w0/static/media/finn.965ed122.png"
+  },
+  {
+    "revision": "74f7d02507837a2b2fc525166c96aff8",
+    "url": "https://.github.io/csb-6lk4w0/static/media/bmo.74f7d025.png"
+  },
+  {
+    "revision": "337a4119783dcab36745ddf4aa9a419d",
+    "url": "https://.github.io/csb-6lk4w0/static/media/princess.337a4119.png"
+  },
+  {
+    "revision": "0599b98e4bc1d271d29535ae1303004f",
+    "url": "https://.github.io/csb-6lk4w0/index.html"
+  }
+];
